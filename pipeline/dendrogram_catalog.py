c1291ed9d8437d8c5fe57f58dac5ef01357862cd
Justin Otter <jotter@nmpost030.aoc.nrao.edu>
2018-07-11 17:04:56 -0600

    general updates

--- pipeline/dendrogram_catalog.py
+++ pipeline/dendrogram_catalog.py
@@ -3,7 +3,7 @@ from region_file import compute_regions
 from gaussfit_catalog import gaussfit_catalog
 from regions import CircleSkyRegion
 
-from astropy.io import fits
+from astropy.io import fits, ascii
 import astropy.units as u
 from astropy.coordinates import Angle, SkyCoord
 from astropy.table import Table, join
@@ -15,23 +15,22 @@ import pylab as pl
 #param_file = '/users/jotter/summer_research_2018/pipeline/data_params.csv'
 
 def create_catalog(param_file):
-	params = np.genfromtxt(param_file, delimiter=',', dtype=[('name', 'U6'), ('fname', 'U150'), ('min_val', '<f8'), ('min_del', '<f8'), ('n_pix', '<i8'), ('pbcorr', 'U138')], skip_header=2)
+	params = ascii.read(param_file)
 
 	catalogs = {}
 
-	for dset in params:
-		name=dset[0]
-		min_val = dset[2]
-		min_del = dset[3]
-		n_pix = dset[4]
-		img = dset[1]
+	for row in range(len(params)):
+		name=params['name'][row]
+		min_val = params['min_value'][row]
+		min_del = params['min_delta'][row]
+		n_pix = params['n_pix'][row]
+		img = params['file_name'][row]
 		reg_fname = '/users/jotter/summer_research_2018/final_regs/'+name+'_reg_file.reg'
 		dendro, cat = compute_regions(min_val, min_del, n_pix, img, reg_fname)
-		if dset[5] != 'True': #if the image is not pb corrected, measure flux from pb corrected image
-			img = dset[5]
+		if params['pbcorr'][row] != 'True': #if the image is not pb corrected, measure flux from pb corrected image
+			img = params['pbcorr'][row]
 			corr_file = fits.open(img)
 			data_corr = corr_file[0].data
-			corr_cat = cat
 			for i,leaf in enumerate(dendro.all_structures):
 			    ind = leaf.indices()
 			    max_y = np.max(ind[0])
@@ -48,29 +47,28 @@ def create_catalog(param_file):
 			    
 			    leaf_data = cutout_corr.data[mask_cut.data==True]
 			    leaf_flux = np.sum(leaf_data)
-			    corr_cat['flux'][i] = leaf_flux
-			cat = corr_cat
-		cat.rename_column('flux', 'flux '+name)
+			    cat['flux'][leaf.idx] = leaf_flux
+		cat.rename_column('flux', 'flux_'+name)
+		cat.rename_column('_idx', '_idx_'+name)
 		#next, measure centroids with gaussian fitting
 		rad = Angle(1, 'arcsecond') #radius for region list
 		#generate region list
 		regs = []
 		for ind in range(len(cat['x_cen'])):
-		    regs.append(CircleSkyRegion(center=SkyCoord(cat['x_cen'][ind]*u.degree, cat['y_cen'][ind]*u.degree), radius=rad, meta={'text':str(cat['_idx'][ind])}))
+		    regs.append(CircleSkyRegion(center=SkyCoord(cat['x_cen'][ind]*u.degree, cat['y_cen'][ind]*u.degree), radius=rad, meta={'text':str(cat['_idx_'+name][ind])}))
 
 		cat_r = Angle(0.3, 'arcsecond') #radius in gaussian fitting
 		pl.close('all')
 		gauss_cat = gaussfit_catalog(img, regs, cat_r, savepath='/lustre/aoc/students/jotter/gauss_diags/'+name) #output is nested dictionary structure
 
-		gauss_fit_tab = Table(names=('_idx', 'gauss_x_'+name, 'gauss_y_'+name, 'FWHM_major_'+name, 'FWHM_minor_'+name, 'position_angle_'+name, 'peak_flux_'+name, 'gauss_amplitude_'+name, 'fit_goodness'), dtype=('i4','f8','f8','f8','f8','f8','f8','f8','U10')) #turn into astropy table
+		gauss_fit_tab = Table(names=('_idx_'+name, 'gauss_x_'+name, 'gauss_y_'+name, 'FWHM_major_'+name, 'FWHM_minor_'+name, 'position_angle_'+name, 'peak_flux_'+name, 'gauss_amplitude_'+name, 'fit_goodness_'+name), dtype=('i4','f8','f8','f8','f8','f8','f8','f8','U10')) #turn into astropy table
 		for key in gauss_cat:
 		    gauss_fit_tab.add_row((key,gauss_cat[key]['center_x'],gauss_cat[key]['center_y'], gauss_cat[key]['fwhm_major'], gauss_cat[key]['fwhm_minor'], gauss_cat[key]['pa'], gauss_cat[key]['peak'], gauss_cat[key]['amplitude'], 'none')) #fill table
 	
-		full_cat = join(cat, gauss_fit_tab, keys='_idx', join_type='outer') #joining the gaussian centroid data with the rest
+		full_cat = join(cat, gauss_fit_tab, keys='_idx_'+name, join_type='outer') #joining the gaussian centroid data with the rest
 	
 		catalogs[name] = full_cat
 
-	#also add column describing reliability of centroid
 	for dat in catalogs:
 		Table(catalogs[dat]).write('/lustre/aoc/students/jotter/dendro_catalogs/'+dat+'_dendro_catalog_all.fits', format='fits', overwrite=True)
 
--- pipeline/match_all_catalogs.py
+++ pipeline/match_all_catalogs.py
@@ -113,85 +113,13 @@ idx, d2d, d3d = dendro_coord.match_to_catalog_sky(ext_coord)
 matched_inds = np.where(d2d.degree < (1/3600))
 
 for all_ind in matched_inds[0]:
-	print('ok')
 	ext_table[idx[all_ind]]['D_ID'] = dendro_table[all_ind]['D_ID']
 
 IR_matched_table = join(dendro_table, ext_table, keys='D_ID', join_type='left', table_names=[names[ind -1], names[ind]])
 IR_matched_table.write('/lustre/aoc/students/jotter/dendro_catalogs/IR_matched_catalog.fits',format='fits',overwrite=True)
 
-'''
-#indices of unmatched sources
-unmatched_inds = np.delete(np.arange(len(d2d)),matched_inds)
-
-#combining matches in dendrogram catalogue and HC coordinates
-d2d_tab = Column(d2d.degree, name='match_dist')
-matched_tab_coord = HC_coord[idx[matched_inds]]
-matched_cat = hstack((Table(dendro_table[matched_inds]), Table(HC_data[idx[matched_inds]])),join_type='outer')
-matched_cat.add_column(d2d_tab[matched_inds], name='match_dist')
-matched_cat['match_dist'].unit = 'degree'
-
-#matched_cat.write('/lustre/aoc/students/jotter/dendro_catalogs/IR_matched_catalog.fits',format='fits',overwrite=True)
-
-#catalogue for unmatched sources
-unmatched_cat = Table(dendro_table[unmatched_inds])
-
-#unmatched_cat.write('/lustre/aoc/students/jotter/unmatched_cat.fits',format='fits')
-
-
-'''
-'''
-#loading in the HC catalog
-col_locs = np.array([0,4,6,9,15,18,21,26,34,43,50,57]) #spacing of input file
-widths = col_locs[1:] - col_locs[:-1]
-HC_data = np.genfromtxt('/lustre/aoc/students/jotter/mr.table1.dat',skip_header=37,delimiter=widths,autostrip=True)
-ID_HC = HC_data[:,0]
-ID_HC = ID_HC.astype('int')
-ID_empty_HC = np.full(len(HC_data[:,0]),'none',dtype='U10')
-#converting from hours,minutes,seconds to degrees
-ra_HC =(HC_data[:,1].astype(float)+HC_data[:,2].astype(float)/60+HC_data[:,3].astype(float)/3600)*(360/24)
-ra_HC = ra_HC*u.degree
-#degrees + arcmins and arcsecs
-dec_HC = HC_data[:,4].astype(float)+(HC_data[:,5].astype(float)/60+HC_data[:,6].astype(float)/3600)*np.sign(HC_data[:,4].astype(float))
-dec_HC = dec_HC*u.degree
-
-HC_table = Table((ID_HC, ID_empty_HC, ra_HC, dec_HC, HC_data[:,7], HC_data[:,8], HC_data[:,9], HC_data[:,10]), names=('HC ID', 'RRS ID', 'RA_HC', 'DEC_HC', 'Kmag', 'Hmag', 'Kmag relative error', 'Hmag relative error'), dtype=('unicode_', 'unicode_', 'float', 'float', 'str', 'str', 'float', 'float'))
-
-#doing the same for the RRS catalog
-RRS_data = fits.getdata('/lustre/aoc/students/jotter/RRS2008_data.fit')
-
-ra_RRS = RRS_data['_RAJ2000']*u.degree
-dec_RRS = RRS_data['_DEJ2000']*u.degree
-ID_RRS = RRS_data['Name'].strip()
-ID_empty_RRS = np.full(len(ID_RRS),'none')
-
-RRS_table = Table((ID_empty_RRS, ID_RRS, ra_RRS, dec_RRS), names=('HC ID', 'RRS ID', 'RA_RRS', 'DEC_RRS'))
-
-#first need to match HC and RRS tables
-HC_coord = SkyCoord(ra=HC_table['RA_HC'], dec=HC_table['DEC_HC'])
-RRS_coord = SkyCoord(ra=RRS_table['RA_RRS'], dec=RRS_table['DEC_RRS'])
-HC_idx, HC_d2d, HC_d3d = HC_coord.match_to_catalog_sky(RRS_coord)
-#match within 0.5 arcsec
-HC_match = np.where(HC_d2d.degree < 0.5*(1/3600))
-#now loop through to add RRS IDs to HC table and vice versa
-for match in HC_match[0]:
-	HC_table[match]['RRS ID'] = RRS_table[HC_idx[match]]['RRS ID']
-	RRS_table[HC_idx[match]]['HC ID'] = HC_table[match]['HC ID']
-
-#ext_table is the combination of RA/decs/IDs from the HC and RRS catalogs
-ext_table = join(HC_table, RRS_table,keys=('HC ID', 'RRS ID'), join_type='outer')
-RA_ext = np.
-ext_coord = SkyCoord(ra=ext_table['RA_ext'], dec=ext_table['DEC_ext'])
-
-#not worrying about the SED text files for now
-
-
-plt.scatter(ext_table['RA_HC'], ext_table['DEC_HC'], c='g',alpha=0.3)
-plt.scatter(ra_my, dec_my, c='r',alpha=0.3)
-plt.scatter(ext_table['RA_RRS'], ext_table['DEC_RRS'], c='k', alpha=0.6)
-plt.xlabel('RA (degrees)')
-plt.ylabel('dec (degrees)')
-plt.show()
-'''
+#IR_source_table = join(dendro_table, ext_table, keys='D_ID', join_type='outer', table_names=[names[ind -1], names[ind]])
+ext_table.write('/lustre/aoc/students/jotter/dendro_catalogs/IR_only_catalog.fits',format='fits',overwrite=True)
 
 '''
 wavelengths = set()
