173ee36757a2d43c415f5e33830de478632b6312
Justin Otter <jotter@nmpost002.aoc.nrao.edu>
2019-04-11 10:16:02 -0600

    updt

--- pipeline/make_paper_tables.py
+++ pipeline/make_paper_tables.py
@@ -2,9 +2,18 @@ from astropy.table import Table
 from astropy.io import fits, ascii
 from astropy.modeling import blackbody
 from astropy import constants
+from astropy.coordinates import SkyCoord
 import radio_beam
 import numpy as np
 import astropy.units as u
+
+
+def RA_to_deg(HH, MM, SS):
+            return (HH + MM/60 + SS/(60*60))*(360/24)
+
+def DEC_to_deg(DD, MM, SS):
+    return DD + (MM/60 + SS/(60*60))*np.sign(DD)
+                
 
 FWHM_TO_SIGMA = 1/np.sqrt(8*np.log(2))
 
@@ -20,16 +29,15 @@ freqs = [98, 223.5 ,339.7672758867]
 #calculated quantities not requiring loops
 A = np.log10(freqs[1]) - np.log10(freqs[0])
 alpha_B3B6 = (np.log10(data['ap_flux_B6'])-np.log10(data['ap_flux_B3']))/A
-alpha_B3B6_err = np.sqrt((data['ap_flux_err_B6']/(A*np.log(10)*data['ap_flux_B6']))**2 + (data['ap_flux_err_B3']/(A*np.log(10)*data['ap_flux_B3']))**2))
+alpha_B3B6_err = np.sqrt((data['ap_flux_err_B6']/(A*np.log(10)*data['ap_flux_B6']))**2 + (data['ap_flux_err_B3']/(A*np.log(10)*data['ap_flux_B3']))**2)
 B = np.log10(freqs[2]) - np.log10(freqs[1])
 alpha_B6B7 = (np.log10(data['ap_flux_B7'])-np.log10(data['ap_flux_B6']))/B
-alpha_B6B7_err = np.sqrt((data['ap_flux_err_B6']/(B*np.log(10)*data['ap_flux_B6']))**2 + (data['ap_flux_err_B7']/(B*np.log(10)*data['ap_flux_B7']))**2))
+alpha_B6B7_err = np.sqrt((data['ap_flux_err_B6']/(B*np.log(10)*data['ap_flux_B6']))**2 + (data['ap_flux_err_B7']/(B*np.log(10)*data['ap_flux_B7']))**2)
 
 #calclated quantites for each band
 int_flux_arrs = []
 int_flux_err_arrs = []
 lower_lum_arrs = []
-lower_lum_err_arrs = []
 mass_arrs = []
 mass_err_arrs = []
 inclination_arrs = []
@@ -70,21 +78,22 @@ for b in range(len(bands)):
     mass_arrs.append(Dmass)
     mass_err_arrs.append(Dmass_err)
 
-    
+
+print(type(inclination_arrs[0]), type(inclination_err_arrs[0]), type(lower_lum_arrs[0]), type(mass_arrs[0]), type(mass_err_arrs[0]), type(alpha_B3B6))
 calc_tab = Table((int_flux_arrs[0], int_flux_err_arrs[0], int_flux_arrs[1], int_flux_err_arrs[1],
-                 int_flux_arrs[2], int_flux_err_arrs[2], inclination_arrs[0], inclination_err_arrs[0],
-                 inclination_arrs[1], inclination_err_arrs[1], inclination_arrs[2], inclination_err_arrs[2],
-                 lower_lum_arrs[0], lower_lum_arrs[1], lower_lum_arrs[2], mass_arrs[0], mass_err_arrs[0],
+                  int_flux_arrs, int_flux_err_arrs, inclination_arrs[0], inclination_err_arrs[0],
+                  inclination_arrs[1], inclination_err_arrs[1], inclination_arrs[2], inclination_err_arrs[2],
+                  lower_lum_arrs[0], lower_lum_arrs[1], lower_lum_arrs[2], mass_arrs[0], mass_err_arrs[0],
                   mass_arrs[1], mass_err_arrs[1], mass_arrs[2], mass_err_arrs[2], alpha_B3B6,
                   alpha_B3B6_err, alpha_B6B7, alpha_B6B7_err),
-                 names=('int_flux_B3', 'int_flux_err_B3', 'int_flux_B6', 'int_flux_err_B6',
-                        'int_flux_B7', 'int_flux_err_B7', 'inclination_B3', 'inclination_err_B3',
-                        'inclination_B6', 'inclination_err_B6', 'inclination_B7', 'inclination_err_B7',
-                        'lower_lum_B3', 'lower_lum_B6', 'lower_lum_B7', 'dust_mass_B3', 'dust_mass_err_B3',
-                        'dust_mass_B6', 'dust_mass_err_B6', 'dust_mass_B7', 'dust_mass_err_B7', 'alpha_B3B6'
-                        'alpha_B3B6_err', 'alpha_B6B7', 'alpha_B6B7_err'),
-                 dtype=('f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8',
-                        'f8','f8','f8','f8','f8','f8','f8','f8','f8','f8'))    
+                  names=('int_flux_B3', 'int_flux_err_B3', 'int_flux_B6', 'int_flux_err_B6',
+                         'int_flux_B7', 'int_flux_err_B7', 'inclination_B3', 'inclination_err_B3',
+                         'inclination_B6', 'inclination_err_B6', 'inclination_B7', 'inclination_err_B7',
+                         'lower_lum_B3', 'lower_lum_B6', 'lower_lum_B7', 'dust_mass_B3', 'dust_mass_err_B3',
+                         'dust_mass_B6', 'dust_mass_err_B6', 'dust_mass_B7', 'dust_mass_err_B7', 'alpha_B3B6'
+                         'alpha_B3B6_err', 'alpha_B6B7', 'alpha_B6B7_err'),
+                 dtype=('f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8','f8',
+                        'f8','f8','f8','f8','f8','f8','f8','f8','f8'))    
 
 EisnerID = np.repeat(-1, len(data))
 
@@ -119,6 +128,16 @@ table2 = Table((data['D_ID'], calc_tab['lower_lum_B3'], calc_tab['lower_lum_B6']
 B6ind = np.where(np.isnan(data['RA_B6']) == False)[0]
 B7ind = np.where(np.isnan(data['RA_B7']) == False)[0]
 allband_ind = np.intersect1d(B6ind, B7ind)
+
+eisner_tab = Table.read('../tables/eisner_tab.txt', format='ascii')
+
+print(eisner_tab['alpha'])
+#eisner_ra = RA_to_deg(eisner_tab['alpha']
+eisner_dec = eisner_tab['delta']
+
+coord_tab = SkyCoord(ra=table1['RA_B3']*u.degree, dec=table1['DEC_B3']*u.degree)
+
+#eisner_coord = SkyCoord(ra=)
 
 table1 = table1[allband_ind]
 table2 = table2[allband_ind]
