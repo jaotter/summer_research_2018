b0bd812a99d8c4ac48cc37099d16422ec7d89004
Justin Otter <jotter@nmpost014.aoc.nrao.edu>
2019-06-11 10:37:02 -0600

    added cos(i) hist)

--- flux_plots/incl_hist.py
+++ flux_plots/incl_hist.py
@@ -15,9 +15,19 @@ for b in range(len(bins[:-1])): #creating points to plot - midpoints of bins
     plotpts.append(bins[b] + (bins[b+1]-bins[b])/2)
     widths.append((bins[b+1]-bins[b]))
 
+#add cos(i) histogram
+tot = np.sum(hist)
+prob = np.abs(np.cos(plotpts))
+prob = prob/np.sum(prob)
+cos_hist = tot*prob
+
+print(np.sum(cos_hist), np.sum(hist))
+
+plt.bar(plotpts, cos_hist, widths, edgecolor='k', alpha=.5, label=r'$\cos(i)$')
 
 
-plt.bar(plotpts, hist, widths, edgecolor='k', alpha=0.5)
+plt.bar(plotpts, hist, widths, edgecolor='k', alpha=0.5, label='band 3')
 plt.xlabel('Band 3 inclination angle (degrees)')
 plt.ylabel('number')
+plt.legend()
 plt.savefig('plots/incl_hist.png',dpi=300)
