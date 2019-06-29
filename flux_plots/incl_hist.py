28cddc89d88d79e25df8643ed759f7e45c177e4b
Justin Otter <jotter@nmpost008.aoc.nrao.edu>
2019-06-28 21:42:11 -0600

    KS test work

--- flux_plots/incl_hist.py
+++ flux_plots/incl_hist.py
@@ -19,25 +19,19 @@ for b in range(len(incl_bins[:-1])): #creating points to plot - midpoints of bin
     plotpts.append(incl_bins[b] + (incl_bins[b+1]-incl_bins[b])/2)
     widths.append((incl_bins[b+1]-incl_bins[b]))
 
-#add cos(i) histogram
+#add sin(i) histogram
 tot = np.sum(hist)
-prob = np.cos(plotpts*u.degree)
+prob = np.sin(plotpts*u.degree)
 prob = prob/np.sum(prob)
-cos_hist = tot*prob
+sin_hist = tot*prob
 
 
-cos_widths = []
-for b in range(len(incl_bins[:-1])):
-    cos_widths.append(np.abs((np.cos(incl_bins[b+1]*u.degree)-np.cos(incl_bins[b]*u.degree))))
-
-print(cos_hist*cos_widths)
-cos_plotpts = np.cos(plotpts*u.degree)
-
-Dval, pval = ks_2samp(cos_hist, hist)
+print(sin_hist)
+Dval, pval = ks_2samp(hist, sin_hist)
 print('KS statistic: %f, p value: %f' % (Dval, pval))
 
 plt.figure()
-plt.bar(plotpts, cos_hist, widths, edgecolor='k', alpha=.5, label=r'$\cos(i)$')
+plt.bar(plotpts, sin_hist, widths, edgecolor='k', alpha=.5, label=r'$\sin(i)$')
 plt.bar(plotpts, hist, widths, edgecolor='k', alpha=0.5, label='band 3')
 plt.xlabel('Band 3 inclination angle (degrees)')
 plt.ylabel('number')
@@ -46,6 +40,7 @@ plt.xlim(0,90)
 plt.savefig('plots/incl_hist.png',dpi=300)
 
 
+#histogram of cos(i)
 
 cos_bins = np.linspace(0,1,10)
 cos_incl_hist, bins = np.histogram(np.cos(dataB3['inclination_B3'][ind]*u.degree), bins=cos_bins)
@@ -66,13 +61,12 @@ print('KS statistic: %f, p value: %f' % (Dval, pval))
 #something wrong here with renaming widths
 print(np.sum(cos_incl_hist*widths))
 print(np.sum(hist*widths))
-print(np.sum(cos_hist*cos_widths))
+#print(np.sum(cos_hist*cos_widths))
 print(np.sum(unif_hist*widths))
 
 plt.figure()
 plt.bar(plotpts, unif_hist, widths, edgecolor='k', alpha=.5, label='uniform')
 plt.bar(plotpts, cos_incl_hist, widths, edgecolor='k', alpha=0.5, label='band 3')
-plt.bar(cos_plotpts, cos_hist, cos_widths, edgecolor='k', alpha=0.5, label='test')
 plt.xlabel('cos(i)')
 plt.ylabel('number')
 plt.legend()
