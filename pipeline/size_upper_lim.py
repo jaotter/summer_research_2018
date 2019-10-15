df53779b04d888e38eaa5c50fb6f6f096f06efff
Justin Otter <jotter@nmpost025.aoc.nrao.edu>
2019-10-15 07:21:46 -0600

    loop working but strange results

--- pipeline/size_upper_lim.py
+++ pipeline/size_upper_lim.py
@@ -11,12 +11,12 @@ import numpy as np
 import regions
 import sys
 
-def draw_circle(r,size,total_flux): #draw circle radius r in 2d rectangular array arr
+def draw_circle(r,size,flux): #draw circle radius r in 2d rectangular array arr
     xx, yy = np.mgrid[:size, :size]
     radius = np.sqrt((xx - size/2)**2 + (yy - size/2)**2)
     circle_arr = np.zeros((size, size))
     circle_ind = np.where(radius < r)
-    circle_arr[circle_ind] = total_flux
+    circle_arr[circle_ind] = flux
     return circle_arr
 
     
@@ -24,63 +24,65 @@ B3file = '/lustre/aoc/students/jotter/directory/OrionB3/Orion_SourceI_B3_continu
 B6file = ''
 B7file = ''
 
-orig_fl = fits.open(B3file)
+img = B3file
+
+radii_as = [0.3,0.275,0.25,0.225,0.2,0.175,0.15,0.125,0.1,0.08,0.06,0.05,0.045,0.04,0.035,0.03,0.025]
+
+orig_fl = fits.open(img)
 
 wcs = WCS(orig_fl[0].header).celestial
 beam = Beam.from_fits_header(orig_fl[0].header)
 
 pixel_scale = np.abs(wcs.pixel_scale_matrix.diagonal().prod())**0.5 * u.deg
 
-#create fake image data - uniform disk with radius r
-r_arcsec = 0.1*u.arcsec #radius in arcsec
-r = (r_arcsec/pixel_scale).decompose() #radius in pixel
-size = 1000 #size of square fake data in pixels
-tot_flux = 1e-4 #total flux of source
-fake_data = draw_circle(r,size,tot_flux)
-
-plt.imshow(fake_data)
-plt.savefig('size_lims/r0.1ascircle.png')
-
 kernel = beam.as_kernel(pixel_scale)
-convolved_fake_img = convolve_fft(fake_data, kernel)
 
-#next step: add noise similar to images
-noise_rms = 5e-5 
-noise_arr = np.random.normal(0, noise_rms, (size,size))
+size=1000 #square size of fake image
+flux=1e-3 #flux of each pixel in fake img
 
-noisy_conv_fake_img = noise_arr + convolved_fake_img
+recovered_fwhm_maj = []
 
-new_fl = orig_fl
-new_fl[0].data = noisy_conv_fake_img
-new_fl.writeto('/users/jotter/summer_research_2018/pipeline/size_lims/fake_conv_img.fits', overwrite=True)
+for rad in radii_as:
+  #create fake image data - uniform disk with radius r
+  r_arcsec = rad*u.arcsec #radius in arcsec
+  r = (r_arcsec/pixel_scale).decompose() #radius in pixel
+  fake_data = draw_circle(r,size,flux)
 
-sys.exit('done')
+  convolved_fake_img = convolve_fft(fake_data, kernel)
 
-loc = wcs.wcs_pix2world(500,500,1)
-reg = regions.CircleSkyRegion(center=SkyCoord(loc[0], loc[1], unit='deg'), radius=0.5*u.arcsec, meta={'text':'test'})
-reg_pix = reg.to_pixel(wcs)
+  #add noise similar to images
+  noise_rms = 5e-5 
+  noise_arr = np.random.normal(0, noise_rms, (size,size))
 
-gaussfit = gaussfit_catalog('/users/jotter/summer_research_2018/fake_conv_img.fits', [reg], Angle(0.2, 'arcsecond'), savepath='/users/jotter/summer_research_2018/')
+  noisy_conv_fake_img = noise_arr + convolved_fake_img
 
-source_size = Beam(major=gaussfit['test']['fwhm_major'], minor=gaussfit['test']['fwhm_minor'], pa=(gaussfit['test']['pa'].value+90)*u.degree)
-try:
-    deconv_size = source_size.deconvolve(B3beam)
-    print('gaussfit deconv major: '+str(gaussfit['test']['deconv_fwjm_major'])+' minor: '+str(gaussfit['test']['deconv_fwhm_minor'])+' deconv + 90 degrees major: '+str(deconv_size.major.value)+' minor: '+str(deconv_size.minor.value))
+  new_fl = orig_fl
+  new_fl[0].data = noisy_conv_fake_img
+  new_fl.writeto('/users/jotter/summer_research_2018/pipeline/size_lims/fake_conv_img.fits', overwrite=True)
 
-except ValueError:
-    print('could not be deconvolved')
-    
-'''
-plt.imshow(convolved_img, cmap='viridis', origin='lower', interpolation='nearevvvvvst')
 
-center_deg = SkyCoord(gaussfit['test']['center_x'], gaussfit['test']['center_y'], unit='deg')
-center_pix = skycoord_to_pixel(center_deg, wcs)
+  loc = wcs.wcs_pix2world(size/2,size/2,1)
+  reg = regions.CircleSkyRegion(center=SkyCoord(loc[0], loc[1], unit='deg'), radius=0.5*u.arcsec, meta={'text':'test'})
+  reg_pix = reg.to_pixel(wcs)
 
-fwhm_maj_deconv_pix = ((gaussfit['test']['deconv_fwhm_major']*u.arcsec).to(u.degree)/pixel_scale).decompose()
-fwhm_min_deconv_pix = ((gaussfit['test']['deconv_fwhm_minor']*u.arcsec).to(u.degree)/pixel_scale).decompose()
-ellipse = patches.Ellipse(center_pix, width=fwhm_min_deconv_pix, height=fwhm_maj_deconv_pix, angle=gaussfit['test']['deconv_pa'])
+  gaussfit = gaussfit_catalog('/users/jotter/summer_research_2018/pipeline/size_lims/fake_conv_img.fits', [reg], Angle(0.2, 'arcsecond'), savepath='/users/jotter/summer_research_2018/pipeline/size_lims/')
 
-plt.add_patch(ellipse)
+  source_size = Beam(major=gaussfit['test']['fwhm_major'], minor=gaussfit['test']['fwhm_minor'], pa=(gaussfit['test']['pa'].value+90)*u.degree)
+  try:
+      deconv_size = source_size.deconvolve(beam)
+      print(deconv_size)
+      recovered_fwhm_maj.append(deconv_size.major.value)
+  except ValueError:
+      print('could not be deconvolved')
+      recovered_fwhm_maj.append(0)
+print(radii_as, recovered_fwhm_maj)
 
-plt.savefig('/users/jotter/summer_research_2018/fake_img_fit.png')
-'''
+plt.figure()
+plt.scatter(radii_as, recovered_fwhm_maj)
+plt.xlabel('disk radius (as)')
+plt.ylabel('recovered major FWHM (as)')
+plt.xlim(0,0.3)
+plt.ylim(0,0.2)
+plt.plot([0,.4],[0,.4])
+
+plt.savefig('size_lims/recov_radius.png')
