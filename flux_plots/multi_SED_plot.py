1d32ce466ac857a0edeafa277f684928f85239ed
Justin Otter <jaotter123@gmail.com>
2020-06-16 14:12:42 -0700

    finished seds

--- flux_plots/multi_SED_plot.py
+++ flux_plots/multi_SED_plot.py
@@ -1,5 +1,6 @@
 from astropy.io import fits, ascii
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from astropy.table import Table
 import numpy as np
 import astropy.units as u
@@ -17,10 +18,11 @@ def get_ind(table): #returns indices where sources are detected in all bands
 
 def plot_SED(ax, srctab):
     freqs = np.array([98, 223.5, 339.7672758867]) #*u.GHz
-    
     alpha = [1.5,2,2.5]
 
-    fluxes = [srctab['ap_flux_B3'], srctab['ap_flux_B6'], srctab['ap_flux_B7']]
+    print(f'creating SED for source {srctab["D_ID"]}')
+    
+    fluxes = [srctab['ap_flux_B3']*1000, srctab['ap_flux_B6']*1000, srctab['ap_flux_B7']*1000]
     flux_errs = [srctab['ap_flux_err_B3'], srctab['ap_flux_err_B6'], srctab['ap_flux_err_B7']]
 
     F2 = fluxes[1]
@@ -32,30 +34,43 @@ def plot_SED(ax, srctab):
         F1 = F2*(freqs/nu2)**alpha[j]
         ax.plot(freqs, F1, linestyle='-', label=r'$\alpha = $'+str(alpha[j]))
 
+    ax.text(0.1,0.82,srctab['D_ID'],transform=ax.transAxes,weight='bold')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_ylabel('aperture flux (Jy)')
-    ax.set_xlabel('frequency (GHz)')
     #ax.legend()
 
     
-def create_multi_plot(tab_path):
+def create_multi_plot(tab_path, ncols=5):
     tab = Table.read(tab_path)
     ind = get_ind(tab)
 
-    cols = 6
-    rows = len(ind)/6
+    cols = ncols
+    rows = len(ind)/ncols
     if rows > int(rows):
         rows = int(rows) + 1
 
     fig, axs = plt.subplots(rows, cols, sharex='all', figsize=(10,10))
 
-    for i in range(len(ind)):
+    for i in range(len(axs.flatten())):
         ax = axs.flatten()[i]
-        srcind = ind[i]
-        plot_SED(ax, tab[srcind])
 
+        if i >= len(ind):
+            ax.tick_params(axis='both',which='both',length=0,labelsize=0)
+            
+        else:
+            srcind = ind[i]
+            plot_SED(ax, tab[srcind])
+
+            if len(axs.flatten()) - i <= ncols:
+                ax.set_xlabel('Frequency (GHz)')
+                ax.set_xticks([100,200,300])
+                ax.set_xticklabels(['100', '200', '300'])
+            if i % ncols == 0:
+                ax.set_ylabel('Flux (mJy)')
+                
+    plt.subplots_adjust(wspace=0.35, hspace=0.3)
     plt.savefig('/home/jotter/nrao/plots/multi_SED_plot.png',dpi=300)
+
     
 create_multi_plot('/home/jotter/nrao/summer_research_2018/tables/r0.5_catalog_bgfit_jun20.fits')
 
