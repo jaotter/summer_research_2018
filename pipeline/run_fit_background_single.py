1f846f55df0b6a0f994c884a446a57d992e457b6
Justin Otter <cv-12578@cvpost008.cv.nrao.edu>
2021-02-04 18:43:35 -0500

    running this

--- pipeline/run_fit_background_single.py
+++ pipeline/run_fit_background_single.py
@@ -1,13 +1,13 @@
 from fit_background import *
 
-src = 1
+src = 20
 fit_bg = False
 
 bg_xmean = 0
 bg_ymean = 0
-bg_xsigma = 30
-bg_ysigma = 30
-zoom = 3
+bg_xsigma = 3
+bg_ysigma = 3
+zoom = 2
 
 band = 'B3'
 
@@ -25,9 +25,13 @@ B3_name = 'B3_huge_bg'
 #B7_img = '/home/jotter/nrao/images/Orion_SourceI_B7_continuum_r0.5.clean0.05mJy.250klplus.deepmask.image.tt0.pbcor.fits'
 #B7_name = 'B7_bg_r0.5.clean.0.05mJy.250klplus'
 
+srcs = [23,25,29,33,36,40,42,43,45,52,54,55,56,57,58,59,61,63,64,65,66,68,69,70,71,74,76,77,79,80,81,83,84,85,86,87,88,89,92,93,94,95,97,98,100,102,103,105,106,107,108,109,110,111,112,117,119,120,121,122,123,124,125]
+
+for src in srcs:
+    fit = fit_source(src, B3_img, B3_name, band, fit_bg=fit_bg, bg_stddev_x=bg_xsigma, bg_stddev_y=bg_ysigma, bg_mean_x=bg_xmean, bg_mean_y=bg_ymean, zoom=3, max_offset_in_beams=5)
 
 
-fit = fit_source(src, B3_img, B3_name, band, fit_bg=fit_bg, bg_stddev_x=bg_xsigma, bg_stddev_y=bg_ysigma, bg_mean_x=bg_xmean, bg_mean_y=bg_ymean, zoom=zoom, max_offset_in_beams=5)
+#fit = fit_source(src, B3_img, B3_name, band, fit_bg=fit_bg, bg_stddev_x=bg_xsigma, bg_stddev_y=bg_ysigma, bg_mean_x=bg_xmean, bg_mean_y=bg_ymean, zoom=zoom, max_offset_in_beams=5)
 #print(fit['fwhm_maj_deconv_B6'], fit['fwhm_maj_deconv_err_B6'])
 #print(fit['RA_B6'], fit['DEC_B6'])
 print(fit)
