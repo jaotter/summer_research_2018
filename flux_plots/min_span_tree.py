3ee64b53538713699866dddfd82e4c37f30028d6
Justin Otter <jaotter123@gmail.com>
2021-03-07 17:28:25 -0500

    update feb21

--- flux_plots/min_span_tree.py
+++ flux_plots/min_span_tree.py
@@ -5,14 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import minimum_spanning_tree
-from scipy.stats import gaussian_kde
+from scipy.stats import gaussian_kde, ks_2samp
 
 
-def plot_MST_hist(coord_list, labels, filename, colors=['tab:blue', 'tab:orange']):
+def plot_MST_hist(coord_list, labels, filename, colors=['tab:blue', 'tab:orange'], KS_test=False):
 
     fig = plt.figure(figsize=(10,6))
+    dists = []
     for i, tab_coord in enumerate(coord_list):
-        
         graph = []
 
         for src_coord in tab_coord:
@@ -25,17 +25,24 @@ def plot_MST_hist(coord_list, labels, filename, colors=['tab:blue', 'tab:orange'
         sep_ind = np.where(mst_graph != 0)
         sep_arr = mst_graph[sep_ind]
 
-        bins = np.arange(0,15.1,1.5)
+        dists.append(sep_arr)
+
+        #bins = np.arange(0,15.1,1.5)
+        bins = np.arange(0,20,1.5)
         print(f'Mean separation: {np.mean(sep_arr)} arcseconds for {labels[i]}')
 
         sep_grid = np.linspace(0,14,100)
-        sep_kde = gaussian_kde(sep_arr)
+        sep_kde = gaussian_kde(sep_arr, bw_method='scott')
         sep_pdf = sep_kde.evaluate(sep_grid)                                                                                                                                                         #to normalize, multiply by bin width and number of points
         sep_est = sep_pdf*len(sep_arr)*(bins[1]-bins[0])
 
         plt.hist(sep_arr, bins=bins, label=labels[i], alpha=0.5, color=colors[i], edgecolor='black')
         plt.plot(sep_grid, sep_est, label=labels[i]+' KDE', color=colors[i])
 
+    if KS_test == True:
+        Dval, pval = ks_2samp(dists[0], dists[1])
+        print(f'{pval} p-value of KS test between first two coord lists')
+        
     plt.legend()
     plt.xlabel('Separation (arcsecond)')
     plt.ylabel('Number')
@@ -44,13 +51,27 @@ def plot_MST_hist(coord_list, labels, filename, colors=['tab:blue', 'tab:orange'
 
 
 
-tab = Table.read('/home/jotter/nrao/summer_research_2018/tables/r0.5_catalog_bgfit_apr20.fits')
-
+tab = Table.read('/home/jotter/nrao/summer_research_2018/tables/r0.5_catalog_bgfit_feb21_ulim.fits')
 tab2 = Table.read('/home/jotter/nrao/tables/eis_coord_table.fits')
 
+IR_tab = Table.read('/home/jotter/nrao/summer_research_2018/tables/IR_matches_MLLA_feb21_full.fits')
+nonIR_src = np.setdiff1d(tab['Seq'], IR_tab['Seq'])
+nonIR_ind = [np.where(tab['Seq']==d_id)[0][0] for d_id in nonIR_src]
+IR_src = IR_tab['Seq']
+IR_ind = [np.where(tab['Seq']==d_id)[0][0] for d_id in IR_src]
+
 tab_coord = SkyCoord(ra=tab['RA_B3'], dec=tab['DEC_B3'], unit=u.degree)
+tab_coord_IR = SkyCoord(ra=tab['RA_B3'][IR_ind], dec=tab['DEC_B3'][IR_ind], unit=u.degree)
+tab_coord_nonIR = SkyCoord(ra=tab['RA_B3'][nonIR_ind], dec=tab['DEC_B3'][nonIR_ind], unit=u.degree)
+
+
 tab_coord2 = SkyCoord(ra=tab2['RA'], dec=tab2['DEC'], unit=u.degree)
 
-coord_list = [tab_coord, tab_coord2]
+coord_list = [tab_coord_IR, tab_coord2]
 
-plot_MST_hist(coord_list, labels=('Band 3', 'E18'), filename='/home/jotter/nrao/plots/separation_hist.pdf')
+plot_MST_hist(coord_list, labels=['Band 3 ONC', 'E18'], filename='/home/jotter/nrao/plots/separation_hist_ONC.pdf', KS_test=True)
+#plot_MST_hist(coord_list, labels=['Band 3 OMC1', 'Band 3 ONC', 'E18'], filename='/home/jotter/nrao/plots/separation_hist_all.pdf', colors=['tab:green', 'tab:blue', 'tab:orange'])
+
+coord_list = [tab_coord_nonIR, tab_coord2]
+
+plot_MST_hist(coord_list, labels=['Band 3 OMC1', 'E18'], filename='/home/jotter/nrao/plots/separation_hist_OMC1.pdf')#, colors=['tab:green', 'tab:blue', 'tab:orange'])
