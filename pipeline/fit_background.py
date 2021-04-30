c6f5dd3dcef77fec9f8461954251fc0933382483
Justin Otter <cv-12578@cvpost050.cv.nrao.edu>
2021-04-30 17:46:31 -0400

    running on new cluster try 2

--- pipeline/fit_background.py
+++ pipeline/fit_background.py
@@ -49,7 +49,7 @@ def fit_source(srcID, img, img_name, band, fit_bg=False, bg_stddev_x=30, bg_stdd
     
     #ref_data_name = '/home/jotter/nrao/summer_research_2018/tables/dendro_ref_catalog_edited.fits'
     #ref_data_name = '/home/jotter/nrao/summer_research_2018/tables/ref_catalog_feb21.fits'
-    ref_data_name = '/lustre/cv/observers/cv-12578/orion_disks/tables/ref_catalog_src117.fits'
+    ref_data_name = '/lustre/cv/observers/cv-12578/orion_disks/tables/ref_catalog_may21.fits'
     ref_data = Table.read(ref_data_name)
     
     fl = fits.open(img)
@@ -70,7 +70,7 @@ def fit_source(srcID, img, img_name, band, fit_bg=False, bg_stddev_x=30, bg_stdd
     #now get ready to fit gaussians
     #start by setting up save directory for images
     #gauss_save_dir = '/home/jotter/nrao/gauss_diags_feb21/fitbg/'+img_name+'/'
-    gauss_save_dir = f'/lustre/cv/observers/cv-12578/orion_disks/gauss_diags_feb21/{img_name}/'
+    gauss_save_dir = f'/lustre/cv/observers/cv-12578/orion_disks/gauss_diags_may21/{img_name}/'
     
     print('saving plots to '+gauss_save_dir)
     if not os.path.exists(gauss_save_dir):
--- pipeline/run_fit_background_newb3.py
+++ pipeline/run_fit_background_newb3.py
@@ -6,7 +6,7 @@ from astropy.table import Table
 B3_img = '/lustre/cv/observers/cv-12578/orion_disks/Orion_SourceI_B3_continuum_r0.5.clean0.05mJy.allbaselines.huge.deepmask.image.tt0.pbcor.fits'
 B3_name = 'B3_huge_bg_cat'
 
-fit_param_tab = Table.read('/lustre/cv/observers/cv-12578/orion_disks/fit_params_b3_may21.csv')
+fit_param_tab = Table.read('/lustre/cv/observers/cv-12578/orion_disks/b3_fit_params_may21.csv')
 catalog = Table.read('/lustre/cv/observers/cv-12578/orion_disks/tables/r0.5_catalog_feb21.fits')
 #catalog.rename_column('D_ID', 'Seq')
 
@@ -23,7 +23,7 @@ for fit_row in fit_param_tab:
         xmean, ymean, xsigma, ysigma = (float(fit_row['xmean']), float(fit_row['ymean']), float(fit_row['xsigma']), float(fit_row['ysigma']))
     fit = fit_source(src, B3_img, B3_name, 'B3', fit_bg=bgfit, bg_stddev_x=xsigma, bg_stddev_y=ysigma, bg_mean_x=xmean, bg_mean_y=ymean, zoom=zoom, max_offset_in_beams=5)
     print(fit)
-    cat_ind = np.where(catalog['D_ID'] == fit['Seq'][0])[0]
+    cat_ind = np.where(catalog['D_ID'] == fit['Seq_B3'][0])[0]
     for nm in fit.colnames:
         catalog[nm][cat_ind] = fit[nm][0]
 
