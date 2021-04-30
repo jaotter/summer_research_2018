e0813e7e588a3db43f944a679d446eb9169643d9
Justin Otter <cv-12578@cvpost050.cv.nrao.edu>
2021-04-30 18:02:04 -0400

    try again

--- pipeline/run_fit_background_newb3.py
+++ pipeline/run_fit_background_newb3.py
@@ -8,7 +8,7 @@ B3_name = 'B3_huge_bg_cat'
 
 fit_param_tab = Table.read('/lustre/cv/observers/cv-12578/orion_disks/b3_fit_params_may21.csv')
 catalog = Table.read('/lustre/cv/observers/cv-12578/orion_disks/tables/r0.5_catalog_feb21.fits')
-#catalog.rename_column('D_ID', 'Seq')
+catalog.rename_column('D_ID', 'Seq')
 
 for fit_row in fit_param_tab:
     #print(fit_row['source'])
@@ -23,11 +23,13 @@ for fit_row in fit_param_tab:
         xmean, ymean, xsigma, ysigma = (float(fit_row['xmean']), float(fit_row['ymean']), float(fit_row['xsigma']), float(fit_row['ysigma']))
     fit = fit_source(src, B3_img, B3_name, 'B3', fit_bg=bgfit, bg_stddev_x=xsigma, bg_stddev_y=ysigma, bg_mean_x=xmean, bg_mean_y=ymean, zoom=zoom, max_offset_in_beams=5)
     print(fit)
-    cat_ind = np.where(catalog['D_ID'] == fit['Seq_B3'][0])[0]
+    cat_ind = np.where(catalog['Seq'] == fit['Seq'][0])[0]
     for nm in fit.colnames:
         catalog[nm][cat_ind] = fit[nm][0]
 
 catalog['pa_B3'] = catalog['pa_B3']%360-90
 #catalog = catalog[np.where(catalog['Seq'] == 117)]
 print(catalog)
+
+catalog.rename_column('Seq', 'Seq_B3')
 catalog.write('/lustre/cv/observers/cv-12578/orion_disks/tables/r0.5_b3_catalog_full_may21.fits',overwrite=True)
